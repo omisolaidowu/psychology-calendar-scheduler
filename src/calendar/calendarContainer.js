90d854c7718daef035da1a335a006ebf9de96790
Omisola Idowu Paul <57432464+omisolaidowu@users.noreply.github.com>
2023-07-15 07:33:50 +0100

    Mobile responsiveness-update two

--- src/calendar/calendarContainer.js
+++ src/calendar/calendarContainer.js
@@ -5,11 +5,9 @@ const MyContainer = ({ className, children }) => {
     return (
       <div className="Calendar-orig" id="Calendar-orig">
         <CalendarContainer className={className}>
-          <div style={{ background: "black", color:"black", 
-          fontFamily: "Lucida Console" }}>
+          <div className="cal-div-first">
           </div>
-          <div className="picker" style={{ position: "relative", background:"black", 
-          fontFamily: "Lucida Console" }}>{children}</div>
+          <div className="picker">{children}</div>
         </CalendarContainer>
       </div>
     );
